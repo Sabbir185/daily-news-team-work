82541c9ce81b3ee2c456c0c29e9f107ea53347c5
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-06-06 21:37:03 +0600

    redux api call success

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { loadData } from './redux/Actions/Actions';
+
 
 function App() {
+
+  const data = useSelector((state) => {
+    console.log(state)
+  })
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadData());
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
